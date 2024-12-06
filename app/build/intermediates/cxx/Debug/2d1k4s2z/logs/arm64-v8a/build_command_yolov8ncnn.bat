4b7decb7715758706be8ef6d386a957ec5a85f71
Vu Nguyen <nguyenquangvu1998@gmail.com>
2024-12-06 13:45:45 +0700

    init

---
+++ app/build/intermediates/cxx/Debug/2d1k4s2z/logs/arm64-v8a/build_command_yolov8ncnn.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk\\cmake\\3.10.2.4988404\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Admin\\Desktop\\cobj-project\\app\\.cxx\\Debug\\2d1k4s2z\\arm64-v8a" ^
+  yolov8ncnn
